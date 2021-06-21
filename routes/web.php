f6d60227ed7df30c41ba39b52ee83e29b2560ee2
Dmitriy Pavlyut <dpavliut@gmail.com>
2021-06-21 18:13:17 +0300

    create adminBlock

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['role:admin'])->prefix('admin')->group(function () {
+    Route::get('/',[App\Http\Controllers\Admin\HomeController::class, 'index']);
+});
