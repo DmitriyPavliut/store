0f2ecfae4d53b6f94961a5ef7e4bddf60216d9db
DmitriyPavliut <dpavliut@gmail.com>
2021-07-26 22:52:51 +0300

    edit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->boolean('accepted')->nullable();
+            $table->boolean('accepted');
         });
     }
 
